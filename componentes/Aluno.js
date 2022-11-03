21af5e81b3277d3fa2e491255bdd79054a2eb2bd
Danilo Ataide <danilosataide@gmail.com>
2022-11-02 21:22:17 -0300

    conflito

--- componentes/Aluno.js
+++ componentes/Aluno.js
@@ -3,6 +3,7 @@ import { useContext, useState } from 'react';
 import { BackgroundContext } from "../context/current-background";
 
 export default function Aluno() {
+  const { currentBackground } = useContext(BackgroundContext);
   const [alunos, setAlunos] = useState([]);
   const [formAlunos, setFormAlunos] = useState({
     nome: '',
@@ -10,28 +11,6 @@ export default function Aluno() {
     cidade: '',
     foto: '',
   });
-
-  // const myDoc = collection(db, "Aluno");
-
-  // useEffect(() => {
-  //   const q = query(collection(db, "Aluno"));
-  //   onSnapshot(q, (querySnapshot) => {
-  //     const result = [];
-  //     querySnapshot.forEach((doc) => result.push({ ...doc.data(), id: doc.id }));
-  //     setAlunos(result);
-  //   });
-  // }, []);
-
-  // const postAluno = (value) => {
-  //   addDoc(myDoc, value)
-  //     .then(() => alert("Aluno Salvo!"))
-  //     .catch((error) => alert(error.message));
-  // }
-
-  // const addToEditMode = (personData) => {
-  //   setIdToEdit(personData.id);
-  //   setFormAlunos(personData);
-  // }
 
   return (
     <View style={{ height: '100%' }}>
