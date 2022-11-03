977147d41a2b7aebff0fb9e7d054194684d4204a
Danilo Ataide <danilosataide@gmail.com>
2022-11-02 21:17:37 -0300

    alt

--- componentes/Aluno.js
+++ componentes/Aluno.js
@@ -2,6 +2,8 @@ import { Text, View, TextInput, Button, FlatList } from 'react-native';
 import { useState } from 'react';
 
 export default function Aluno() {
+
+  // const [idToEdit, setIdToEdit] = useState();
   const [alunos, setAlunos] = useState([]);
   const [formAlunos, setFormAlunos] = useState({
     nome: '',
@@ -9,6 +11,28 @@ export default function Aluno() {
     cidade: '',
     foto: '',
   });
+
+  // const myDoc = collection(db, "Aluno");
+
+  // useEffect(() => {
+  //   const q = query(collection(db, "Aluno"));
+  //   onSnapshot(q, (querySnapshot) => {
+  //     const result = [];
+  //     querySnapshot.forEach((doc) => result.push({ ...doc.data(), id: doc.id }));
+  //     setAlunos(result);
+  //   });
+  // }, []);
+
+  // const postAluno = (value) => {
+  //   addDoc(myDoc, value)
+  //     .then(() => alert("Aluno Salvo!"))
+  //     .catch((error) => alert(error.message));
+  // }
+
+  // const addToEditMode = (personData) => {
+  //   setIdToEdit(personData.id);
+  //   setFormAlunos(personData);
+  // }
 
   return (
     <View>
